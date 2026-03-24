05a6631bfbb3b7e5094c3454c20ccc35552a6505
OmarRawas <omar.rawas17@gmail.com>
2026-03-24 03:21:18 +0300

    118

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ val keystoreProperties = Properties().apply {
 android {
     namespace = "com.doraty.app"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
