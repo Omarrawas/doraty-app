ef6aac30ff32635640543c119dfe4474df8b9ebd
Omarrawas <omarrawas@example.com>
2026-04-09 00:40:51 +0300

    57

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,6 +6,16 @@ allprojects {
             url = uri("https://storage.googleapis.com/download.flutter.io")
         }
     }
+    configurations.all {
+        resolutionStrategy {
+            force("androidx.browser:browser:1.8.0")
+            force("androidx.core:core:1.15.0")
+            force("androidx.core:core-ktx:1.15.0")
+            force("androidx.activity:activity:1.10.0")
+            force("androidx.activity:activity-ktx:1.10.0")
+            force("androidx.navigationevent:navigationevent-android:1.0.0")
+        }
+    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
